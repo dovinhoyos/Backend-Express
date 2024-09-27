f8ecb516d432964e039d1428cd741bb90289e5f2
dovinhoyos <dovricarbhoy@gmail.com>
2024-09-27 10:59:59 -0500

    se agrego la configuracion de cors con angular

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: '',
     database: 'backend',
 
 });
--- src/config/server.js
+++ src/config/server.js
@@ -1,11 +1,20 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const usuarioRoutes = require('../routes/usuario.routes')
 
 const Backend = express();
 const port = 3002;
-Backend.use(express.json());
 
+const corsOptions = {
+    origin: 'http://localhost:4200', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+};
+
+Backend.use(cors(corsOptions));
+Backend.use(bodyParser.json());
 Backend.use(usuarioRoutes);
 
 
